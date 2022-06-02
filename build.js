40d70827661c096279ccffab9fc01b9d06cda7ad
Your Name <[email protected]>
2022-06-02 13:01:52 +1200

    Update build script to use the path.join function

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ var jtangle = require ('./out/jtangle');
 var md = require('markdown-it');
 var mdConcatCode = require('markdown-it-concat-code');
 var frontMatter = require('markdown-it-front-matter');
+const path = require('path');
 
 var src = {};
 var mdlang;
@@ -9,8 +10,8 @@ var mdlang;
 exports.run = function (mdFile, outDir) {
     if (mdFile) {
         const fn = path.parse(mdFile).name;
-        const wFile = outDir ? outDir + '/' + fn + '.w' : fn + '.w';
-        const outFile = outDir ? outDir + '/' + fn : fn;
+        const wFile = outDir ? path.join(outDir, fn + '.w') : fn + '.w';
+        const outFile = outDir ? path.join(outDir, fn) : fn;
         
         const content = fs.readFileSync(mdFile).toString();
 
